ec6c5e6ae42b754cfe40f0d3f8375732ceefa744
Lucake <trindaderaposo@hotmail.com>
2024-12-27 13:58:13 -0300

    moved form data to a separate module

---
+++ modules/form_data.js
@@ -0,0 +1,81 @@
+export const initialForm = {
+  url: "https://docs.google.com/forms/d/e/1FAIpQLSftFYtf4wGYeHQq4oiqiUa2m-pXNkO7yZlyM7ztVeKarpWLgQ/formResponse",
+  fields: [
+    {
+      name: "nome",
+      code: "entry.1596959173",
+      type: "input",
+    },
+    {
+      name: "telefone",
+      code: "entry.158845610",
+      type: "input",
+    },
+    {
+      name: "email",
+      code: "entry.1842501334",
+      type: "input",
+    },
+    {
+      name: "projeto",
+      code: "entry.713884241",
+      type: "input",
+    },
+  ],
+};
+
+export const fullForm = {
+  url: "https://docs.google.com/forms/d/1zInM1Zf89cwUPrMa5ZMGQ4nz4akZjRLxSRr8aEMPf8o/formResponse",
+  fields: [
+    {
+      name: "nome-2",
+      code: "entry.1036168316",
+      type: "input",
+    },
+    {
+      name: "telefone-2",
+      code: "entry.1974084074",
+      type: "input",
+    },
+    {
+      name: "projeto-2",
+      code: "entry.1673520773",
+      type: "input",
+    },
+    {
+      name: "email-2",
+      code: "entry.809244251",
+      type: "input",
+    },
+    {
+      name: "nascimento",
+      code: "entry.1546868978",
+      type: "input",
+    },
+    {
+      name: "estado",
+      code: "entry.489358037",
+      type: "input",
+    },
+    {
+      name: "cidade",
+      code: "entry.705058959",
+      type: "input",
+    },
+    {
+      name: "escolaridade",
+      code: "entry.511299699",
+      type: "input",
+    },
+    {
+      name: "como-soube",
+      code: "entry.322928398",
+      type: "input",
+    },
+    {
+      name: "turma",
+      code: "entry.1479635839",
+      type: "input",
+    },
+  ],
+};
--- script.js
+++ script.js
@@ -1,87 +1,9 @@
-const url =
-  "https://docs.google.com/forms/d/e/1FAIpQLSftFYtf4wGYeHQq4oiqiUa2m-pXNkO7yZlyM7ztVeKarpWLgQ/formResponse"; //action url
-const url2 =
-  "https://docs.google.com/forms/d/1zInM1Zf89cwUPrMa5ZMGQ4nz4akZjRLxSRr8aEMPf8o/formResponse";
+import { initialForm, fullForm } from "./modules/form_data.js";
+
 const form = document.querySelector("#form");
 const form2 = document.querySelector("#form2");
 const CONTAINER1 = document.querySelector("#container1");
 const CONTAINER2 = document.querySelector("#container2");
-
-const formFields = [
-  {
-    name: "nome",
-    code: "entry.1596959173",
-    type: "input",
-  },
-  {
-    name: "telefone",
-    code: "entry.158845610",
-    type: "input",
-  },
-  {
-    name: "email",
-    code: "entry.1842501334",
-    type: "input",
-  },
-  {
-    name: "projeto",
-    code: "entry.713884241",
-    type: "input",
-  },
-];
-
-const formFields2 = [
-  {
-    name: "nome-2",
-    code: "entry.1036168316",
-    type: "input",
-  },
-  {
-    name: "telefone-2",
-    code: "entry.1974084074",
-    type: "input",
-  },
-  {
-    name: "projeto-2",
-    code: "entry.1673520773",
-    type: "input",
-  },
-  {
-    name: "email-2",
-    code: "entry.809244251",
-    type: "input",
-  },
-  {
-    name: "nascimento",
-    code: "entry.1546868978",
-    type: "input",
-  },
-  {
-    name: "estado",
-    code: "entry.489358037",
-    type: "input",
-  },
-  {
-    name: "cidade",
-    code: "entry.705058959",
-    type: "input",
-  },
-  {
-    name: "escolaridade",
-    code: "entry.511299699",
-    type: "input",
-  },
-  {
-    name: "como-soube",
-    code: "entry.322928398",
-    type: "input",
-  },
-  {
-    name: "turma",
-    code: "entry.1479635839",
-    type: "input",
-  },
-];
 
 window.addEventListener("DOMContentLoaded", (event) => {
   document.querySelector("body").classList.remove("preload");
@@ -94,13 +16,13 @@ window.onbeforeunload = function () {
 //form 1
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  fetch(url, {
+  fetch(initialForm.url, {
     method: "POST",
     mode: "no-cors",
     header: {
       "Content-Type": "application/json",
     },
-    body: getInputData(formFields),
+    body: getInputData(initialForm.fields),
   })
     .then((data) => {
       console.log(data);
@@ -114,13 +36,13 @@ form.addEventListener("submit", (e) => {
 form2.addEventListener("submit", (e) => {
   e.preventDefault();
   CONTAINER2.classList.add("loading");
-  fetch(url2, {
+  fetch(fullForm.url, {
     method: "POST",
     mode: "no-cors",
     header: {
       "Content-Type": "application/json",
     },
-    body: getInputData(formFields2),
+    body: getInputData(fullForm.fields),
   })
     .then((data) => {
       console.log(data);
